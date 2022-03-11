8480c8d2122e4e53638856cfce5ccf2a1f86668f
Lucas Ferreira <lucticas@gmail.com>
2022-03-11 17:20:42 -0300

    Day 03 -- API fetch for Weather

--- Projeto 03/script.js
+++ Projeto 03/script.js
@@ -46,13 +46,16 @@ function showWarning(message) {
 }
 
 function showInfo(json) {
+
     showWarning('');
-    document.querySelector('.resultado').style.display = 'block';
+
 
     document.querySelector('.titulo').innerHTML = `${json.name} , ${json.country}`;
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
-
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle}deg)`
+
+
+    document.querySelector('.resultado').style.display = 'block';
 }
